cf55b769852b224025314a211a763e1b99ae1c5d
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-01-22 14:25:39 -0800

    Adding delete button

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -5,11 +5,13 @@ import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
 
 
-function ExpenseItem({description, amount, date}) {
+function ExpenseItem({id, description, amount, date}) {
     const navigation = useNavigation();
 
     function expensePressHandler() {
-        navigation.navigate('ManageExpenses');
+        navigation.navigate('ManageExpenses', {
+            expenseId: id
+        });
     }
 
     return (
