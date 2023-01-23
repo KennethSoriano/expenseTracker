4951ede31b24c410d4cb2e1db494af5dd0c1c632
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-01-22 17:41:26 -0800

    Adding custom buttons

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,5 +1,6 @@
 import { useLayoutEffect } from "react";
 import { StyleSheet, View } from "react-native";
+import Button from "../components/UI/Button";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 
@@ -17,8 +18,20 @@ function ManageExpense({route, navigation}) {
 
     }
 
+    function cancelHandler() {
+
+    }
+
+    function confirmHandler() {
+
+    }
+
     return (
         <View style={styles.container}>
+            <View style={styles.buttons}>
+                <Button style={styles.button} mode="flat" onPress={cancelHandler}>Cancel</Button>
+                <Button style={styles.button}>{isEditing ? 'update' : 'Add'}</Button>
+            </View>
             {isEditing && (
                 <View styles={styles.deleteContainer}>
                     <IconButton 
@@ -40,6 +53,15 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 24,
         backgroundColor: GlobalStyles.colors.primary800
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        minwidth: 120,
+        marginHorizontal: 8
     },
     deleteContainer: {
         marginTop: 16,
